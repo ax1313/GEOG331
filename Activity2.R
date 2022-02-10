2dbb7c7d1bfddea8fadbe16399333117ace19f8f
Andrew Xie <axie@colgate.edu>
2022-02-10 02:26:48 -0500

    Added file code to HW2 file

--- Activity2.R
+++ Activity2.R
@@ -4,6 +4,9 @@
 # Make a vector of tree heights in meters
 heights <- c(30, 41, 20, 22)
 
+#get more info on the matrix function
+#help(matrix)
+
 #convert to cm
 heights_cm = heights * 100
 heights_cm
@@ -12,3 +15,20 @@ heights_cm
 #first argument is the vector of numbers to fill in the matrix
 Mat<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=TRUE)
 Mat
+
+#set up a matrix that fills in by columns
+#first argument is the vector of numbers to fill in the matrix
+Mat.bycol<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=FALSE)
+Mat.bycol
+
+#subset the matrix to look at row 1, column2
+Mat.bycol[1,2]
+
+#look at all values in row 1
+Mat.bycol[1,]
+
+#look at all values in column 2
+Mat.bycol[,2]
+
+datW <- read.csv("data/noaa_weather/2011124.csv", stringsAsFactors = T)
+str(datW)
