5e941d9ef9cf9bcaa90296203c5742c8fd793da5
Andrew Xie <axie@colgate.edu>
2022-02-28 04:02:21 -0500

    Finished Activity 3

--- Activity3.R
+++ Activity3.R
@@ -1,4 +1,7 @@
-# COMMENT YOUR CODE!!!!!
+# GEOG331 Activity 3 Script
+# Andrew Xie
+# 02/28/22
+
 library(lubridate)
 
 #create a function. The names of the arguments for your function will be in parentheses. Everything in curly brackets will be run each time the function is run.
@@ -7,19 +10,7 @@ assert <- function(statement,err.message){
   if(statement == FALSE){
     print(err.message)
   }
-  
 }
-
-#check how the statement works
-#evaluate a false statement
-assert(1 == 2, "error: unequal values")
-
-#evaluate a true statement
-assert(2 == 2, "error: unequal values")
-#set up assert to check if two vectors are the same length
-a <- c(1,2,3,4)
-b <- c(8,4,5)
-assert(length(a) == length(b), "error: unequal length")
 
 #read in the data file
 #skip the first 3 rows since there is additional column info
@@ -54,22 +45,6 @@ datW$hour <- hour(dates) + (minute(dates)/60)
 datW$DD <- datW$doy + (datW$hour/24)
 #quick preview of new date calculations
 datW[1,]
-
-#see how many values have missing data for each sensor observation
-#air temperature
-length(which(is.na(datW$air.temperature)))
-
-#wind speed
-length(which(is.na(datW$wind.speed)))
-
-#precipitation
-length(which(is.na(datW$precipitation)))
-
-#soil temperature
-length(which(is.na(datW$soil.moisture)))
-
-#soil moisture
-length(which(is.na(datW$soil.temp)))
 
 #make a plot with filled in points (using pch)
 #line lines
@@ -123,34 +98,72 @@ points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
 datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
                           ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
 
-
-mrg <- merge(datW$DD, datW$DD[lightscale > 0])
-nrow(mrg)
-mrg
-
 # Start QUESTION 5 Here
 assert(all(datW$DD[lightscale > 0] %in% datW$DD), "Error: Cannot subset datW w/ lightscale")
 #End QUESTION 5 Here
 
 # Start QUESTION 6 Here
-
 # Make initial plot to detect suspect wind speed values
 plot(datW$DD, datW$wind.speed, pch=19, type="b", xlab = "Day of Year",
      ylab="Wind speed (m/s)")
 
+# Eliminate extraneous wind speed values
 datW$wind.speed.tempQ1 <- ifelse(datW$wind.speed > 0.7 & datW$DD < 200, NA,
                                  ifelse(datW$wind.speed > 1.2 & datW$DD > 200, NA, 
-                                 ifelse(datW$wind.speed < 0.5 & datW$DD > 205, NA, datW$wind.speed)
+                                 ifelse(datW$wind.speed < 0.5 & datW$DD > 205, NA, datW$wind.speed)))
 
+# Check if new temp wind speed column is not the same as the original wind speed column
 assert(!setequal(datW$wind.speed, datW$wind.speed.tempQ1), "Error: Data not filtered")
 
-length(datW$wind.speed[datW$wind.speed > 3*mean(datW$wind.speed)])
-
+# Make line / point plot (Day of Year vs. Wind Speed)
 plot(datW$DD, datW$wind.speed.tempQ1, pch=19, type="b", xlab = "Day of Year",
      ylab="Wind speed (m/s)")
-
 # End QUESTION 6 Here
 
+# Start QUESTION 7 Here
+# Make 4 plots at a time
+par(mfrow = c(2,2))
 
+# Generate four plots
+plot(datW$DD, datW$soil.temp, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil temp (degrees C)")
 
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil moisture (degrees C)")
 
+# Precipitation contains a few extreme values to be removed
+datW$precipitation.tempQ1 <- ifelse(datW$precipitation > 6, NA, datW$precipitation)
+
+plot(datW$DD, datW$precipitation.tempQ1, pch=19, type="b", xlab = "Day of Year",
+     ylab="Precipitation (mm)")
+
+plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air temperature (degrees C)")
+# End QUESTION 7 Here
+
+# Start QUESTION 8 Here
+num_calc = length(datW$air.temperature[!is.na(datW$soil.moisture)]) + length(datW$precipitation[!is.na(datW$soil.moisture)]) +
+  length(datW$soil.moisture[!is.na(datW$soil.moisture)]) + length(datW$soil.temp[!is.na(datW$soil.temp)])
+
+min_day = min(datW$timestamp)
+max_day = max(datW$timestamp[!is.na(datW$soil.moisture)])
+
+# End QUESTION 8 Here
+
+# Start QUESTION 9 Here
+# Make 4 plots at a time
+par(mfrow = c(2,2))
+
+# Generate four plots
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil moisture (degrees C)")
+
+plot(datW$DD, datW$soil.temp, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil temp (degrees C)")
+
+plot(datW$DD, datW$precipitation, pch=19, type="b", xlab = "Day of Year",
+     ylab="Precipitation (mm)")
+
+plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air temperature (degrees C)")
+# End QUESTION 9 Here
