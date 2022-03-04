7e041ad2ba6227b46a966c4ca8b17ac55e1755d7
Andrew Xie <axie@colgate.edu>
2022-03-04 04:00:45 -0500

    Edited code for A3

--- Activity3.R
+++ Activity3.R
@@ -46,59 +46,9 @@ datW$DD <- datW$doy + (datW$hour/24)
 #quick preview of new date calculations
 datW[1,]
 
-#make a plot with filled in points (using pch)
-#line lines
-plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
-     ylab="Soil moisture (cm3 water per cm3 soil)")
-
-#make a plot with filled in points (using pch)
-#line lines
-plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
-     ylab="Air temperature (degrees C)")
-
-#I'm going to make a new column to work with that indicates that I am conducting QAQC
-#because overwriting values should be done cautiously and can lead to confusing issues.
-#It can be particularly confusing when you are just learning R.
-#Here I'm using the ifelse function
-#the first argument is a logical statement to be evaluated as true or false on a vector
-#the second argument is the value that my air.tempQ1 column will be given if the statement
-#is true. The last value is the value that will be given to air.tempQ1 if the statement is false.
-#In this case it is just given the air temperature value
-datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
-
-#check the values at the extreme range of the data
-#and throughout the percentiles
-quantile(datW$air.tempQ1)
-
-#look at days with really low air temperature
-datW[datW$air.tempQ1 < 8,]  
-
-#look at days with really high air temperature
-datW[datW$air.tempQ1 > 33,]  
-
-#plot precipitation and lightning strikes on the same plot
+# Start QUESTION 5 Here
 #normalize lighting strikes to match precipitation
 lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
-#make the plot with precipitation and lightning activity marked
-#make it empty to start and add in features
-plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation & lightning",
-     type="n")
-#plot precipitation points only when there is precipitation 
-#make the points semi-transparent
-points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 0],
-       col= rgb(95/255,158/255,160/255,.5), pch=15)        
-
-#plot lightning points only when there is lightning     
-points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
-       col= "tomato3", pch=19)
-
-#filter out storms in wind and air temperature measurements
-# filter all values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm.    
-#create a new air temp column
-datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
-                          ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
-
-# Start QUESTION 5 Here
 assert(all(datW$DD[lightscale > 0] %in% datW$DD), "Error: Cannot subset datW w/ lightscale")
 #End QUESTION 5 Here
 
@@ -113,7 +63,8 @@ datW$wind.speedQ1 <- ifelse(datW$wind.speed > 0.7 & datW$DD < 200, NA,
                                  ifelse(datW$wind.speed < 0.5 & datW$DD > 205, NA, datW$wind.speed)))
 
 # Check if new temp wind speed column is not the same as the original wind speed column
-assert(!setequal(datW$wind.speed, datW$wind.speedQ1), "Error: Data not filtered")
+#assert(!setequal(datW$wind.speed, datW$wind.speedQ1), "Error: Data not filtered")
+assert(all(is.na(datW$wind.speedQ1[(datW$wind.speed > 0.7 & datW$DD < 200) | (datW$wind.speed > 1.2 & datW$DD > 200) | (datW$wind.speed < 0.5 & datW$DD > 205)])), "Error: Data not filtered")
 
 # Make line / point plot (Day of Year vs. Wind Speed)
 plot(datW$DD, datW$wind.speedQ1, pch=19, type="b", xlab = "Day of Year",
@@ -129,7 +80,7 @@ plot(datW$DD, datW$soil.temp, pch=19, type="b", xlab = "Day of Year",
      ylab="Soil temp (degrees C)")
 
 plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
-     ylab="Soil moisture (m^3 / m^3)")
+     ylab="Soil moisture (m^3 water / m^3 soil)")
 
 # Precipitation contains a few extreme values to be removed
 datW$precipitationQ1 <- ifelse(datW$precipitation > 6, NA, datW$precipitation)
@@ -142,7 +93,7 @@ plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
 # End QUESTION 7 Here
 
 # Start QUESTION 8 Here
-num_calc = length(datW$air.tempQ1) + length(datW$wind.speedQ1) + length(datW$precipitationQ1)
+num_calc = length(datW$air.temp) + length(datW$wind.speedQ1) + length(datW$precipitationQ1)
   length(datW$soil.moisture[!is.na(datW$soil.moisture)]) + length(datW$soil.temp[!is.na(datW$soil.temp)])
 
 # Get earliest and latest days data was collected
@@ -152,7 +103,7 @@ max_day_soil = max(datW$timestamp[!is.na(datW$soil.moisture)])
 max_day = max(datW$timestamp[is.na(datW$soil.moisture)])
 
 # Find average air temperature (Accuracy = +/- 0.6 degrees C)
-avg_air_temp = mean(datW$air.tempQ1, na.rm = TRUE)
+avg_air_temp = mean(datW$air.temp, na.rm = TRUE)
 
 #Find average wind speed (Accuracy = The greater of 0.3 m/s or 3% of measurement)
 avg_wind_speed = mean(datW$wind.speedQ1, na.rm = TRUE)
@@ -178,7 +129,7 @@ par(mfrow = c(2,2))
 
 # Generate four plots
 plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
-     ylab="Soil moisture (m^3 / m^3)")
+     ylab="Soil moisture (m^3 water / m^3 soil)")
 
 plot(datW$DD, datW$soil.temp, pch=19, type="b", xlab = "Day of Year",
      ylab="Soil temp (degrees C)")
@@ -186,6 +137,6 @@ plot(datW$DD, datW$soil.temp, pch=19, type="b", xlab = "Day of Year",
 plot(datW$DD, datW$precipitationQ1, pch=19, type="b", xlab = "Day of Year",
      ylab="Precipitation (mm)")
 
-plot(datW$DD, datW$air.tempQ1, pch=19, type="b", xlab = "Day of Year",
+plot(datW$DD, datW$air.temp, pch=19, type="b", xlab = "Day of Year",
      ylab="Air temperature (degrees C)")
 # End QUESTION 9 Here
