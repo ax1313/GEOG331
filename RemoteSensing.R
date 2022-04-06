7e0d351b6f810dfdc6878333baf7dd3d0948b50b
ax1313 <axie@colgate.edu>
2022-04-06 12:08:14 -0400

    Generated RGB Graph

--- RemoteSensing.R
+++ RemoteSensing.R
@@ -1,2 +1,16 @@
 # load the terra package
 library(terra)
+
+#set working directory
+#setwd("Z:/GEOG331_S22/students/axie/GEOG331")
+
+# read a raster from file
+p <- rast("Z:/data/rs_data/20190706_002918_101b_3B_AnalyticMS_SR.tif")
+
+# plot the raster
+plot(p)
+
+# plot an RGB rendering of the data
+plotRGB(p, r = 3, g = 2, b = 1,
+        scale = 65535,
+        stretch = "hist")
