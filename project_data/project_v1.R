24f061f613a20fa7ffa17c8b9460cb773941efe6
ax1313 <axie@colgate.edu>
2022-04-15 12:08:45 -0400

    Project data

--- project_data/project_v1.R
+++ project_data/project_v1.R
@@ -16,3 +16,7 @@ nc_data <- nc_open('Z:/students/axie/project/air.2x2.250.mon.anom.comb.nc')
 lon <- ncvar_get(nc_data, "lon")
 lat <- ncvar_get(nc_data, "lat", verbose = F)
 t <- ncvar_get(nc_data, "time")
+
+ndvi.array <- ncvar_get(nc_data, "air") # store the data in a 3-dimensional array
+dim(ndvi.array) 
+
