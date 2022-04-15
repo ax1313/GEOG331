a7b1880c5f898f8567716ad8d6ed7bd4603d29da
ax1313 <axie@colgate.edu>
2022-04-15 12:04:38 -0400

    Project data

--- project_data/project_v1.R
+++ project_data/project_v1.R
@@ -5,10 +5,14 @@ library(terra) # package for raster manipulation
 library(rgdal) # package for geospatial analysis
 library(ggplot2) # package for plotting
 
-nc_data <- nc_open('project_data/air.2x2.250.mon.anom.comb.nc')
+nc_data <- nc_open('Z:/students/axie/project/air.2x2.250.mon.anom.comb.nc')
 # Save the print(nc) dump to a text file
 {
   sink('project_data_v1.txt')
   print(nc_data)
   sink()
 }
+
+lon <- ncvar_get(nc_data, "lon")
+lat <- ncvar_get(nc_data, "lat", verbose = F)
+t <- ncvar_get(nc_data, "time")
