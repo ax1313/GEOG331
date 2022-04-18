f2d3ae021ea833a4a99be3b7d91fb5f6526343e3
ax1313 <axie@colgate.edu>
2022-04-18 12:10:59 -0400

    Remote sensing

--- RemoteSensing.R
+++ RemoteSensing.R
@@ -40,4 +40,33 @@ plot(b2, add = T)
 # buffer the extent by 200m
 b3 <- buffer(b2, width = 200)
 
+# use this to crop the raster layer so we can see just our study area
+p2 <- crop(p, b2, filename = "20190706_SR_crop.tif", overwrite = TRUE)
 
+# make a plot to see how it looks
+plotRGB(p2, r = 3, g = 2, b = 1,
+        scale = 65535,
+        stretch = "lin")
+
+# calculate NDVI
+ndvi <- (p2[[4]]-p2[[3]])/(p2[[4]]+p2[[3]])
+
+# set the layer name to ndvi to avoid confusion
+names(ndvi) = "ndvi"
+
+#create a plot of the ndvi map with sample points on top
+png(filename = "ndvi_map.png", width = 6, height = 4, units = "in", res = 300)
+
+plot(ndvi)
+points(gtree2, cex = gtree$cc.pct/50, col = "blue")
+
+dev.off()
+
+# extract NDVI values for each point
+nt <- terra::extract(ndvi, gtree2, fun = mean, method = 'bilinear')
+
+# plot ndvi vs. canopy cover
+plot(nt$ndvi, gtree2$cc.pct, pch = 16, col = "blue")
+
+# plot again but fix the y-axis
+plot(nt$ndvi.gtree2$cc.pct, pch = 16, col = "blue", xlim = c(0,1), ylim = c(0, 1000))
