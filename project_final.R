2b5b773541ed2e95de400616e76742f2c6cee345
ax1313 <axie@colgate.edu>
2022-05-10 14:15:31 -0400

    Added new tidync package

---
+++ project_final.R
@@ -0,0 +1,7 @@
+
+
+library(ncdf4) # package for netcdf manipulation
+library(raster) # eventually replace with terra
+library(tidync) # package for raster / terra manipulation
+library(rgdal) # package for geospatial analysis
+library(ggplot2) # package for plotting
