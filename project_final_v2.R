4b28f084b44a682e7a6757b06018848392d11a40
ax1313 <axie@colgate.edu>
2022-05-12 23:32:27 -0400

    Final submission

--- project_final_v2.R
+++ project_final_v2.R
@@ -1,3 +1,7 @@
+# GEOG331 Project Script
+# Andrew Xie
+# 05/12/22
+
 # Code adapted from: https://rpubs.com/boyerag/297592
 
 library(ncdf4) 
@@ -8,13 +12,13 @@ library(ggplot2)
 # Temperature (monthly mean) data (air.mon.mean.nc)
 # Data starts from January 1, 1948 and ends on March 1, 2022
 
-# load in file and llongitude, latitude, and time values
+# load in file and longitude, latitude, and time values
 nc_mean_temp <- nc_open('Z:/students/axie/project/air.mon.mean.nc')
 lon <- ncvar_get(nc_mean_temp, "lon")
 lat <- ncvar_get(nc_mean_temp, "lat", verbose = F)
 t <- ncvar_get(nc_mean_temp, "time")
 
-air.array <- ncvar_get(nc_mean_temp, "air") # store the data in a 3-dimensional array
+air.array <- ncvar_get(nc_mean_temp, "air") # store data in 3D array
 fillvalue <- ncatt_get(nc_mean_temp, "air", "_FillValue")
 
 nc_close(nc_mean_temp)
@@ -23,14 +27,14 @@ time_length = nc_mean_temp[["dim"]][["time"]][["len"]]
 
 get_month_vals <- function(first_month_val, lat_val, lon_val, array, length) {
   new_lon_val = round((180 - lon_val) / 2.5) + 1 # Get index value of longitude to extract from air.diff
-  second_month_val <- first_month_val + 12
+  second_month_val <- first_month_val + 12 # Start measurements from next year but with same calendar month
   air.first <- array[, , first_month_val]
   temp <- rep()
   while (second_month_val <= length) {
-    air.last <- air.array[, , second_month_val] # place loop value here
-    air.diff <- air.last - air.first
+    air.last <- air.array[, , second_month_val]
+    air.diff <- air.last - air.first # Difference between two months
     temp <- append(temp, air.diff[new_lon_val, lat_val])
-    second_month_val <- second_month_val + 12
+    second_month_val <- second_month_val + 12 # Keep calendar month consistent
   }
   return(temp)
 }
@@ -45,17 +49,17 @@ plot_vals <- function(month, month_vals, title) {
   
 # Adjust these values for different latitude / longitude 
 
-# # Arctic Coordinates
-# lat_adjust = 6 # Index value corresponding to closest latitude
-# lon_adjust = 100.1140
+# Arctic Coordinates
+lat_adjust = 6 # Index value corresponding to closest latitude
+lon_adjust = 100.1140
 
 # # North Pole
 # lat_adjust <- 1
 # lon_adjust <-  135
 
-# Hamilton
-lat_adjust <- 20
-lon_adjust <-  75.5446
+# # Hamilton
+# lat_adjust <- 20
+# lon_adjust <-  75.5446
 
 # # Miami
 # lat_adjust <- 27
@@ -101,7 +105,7 @@ plot_vals(9, temp_sep, "September Difference in Temperatures from 1948")
 temp_oct <- get_month_vals(10, lat_adjust, lon_adjust, air.array, time_length)
 plot_vals(10, temp_oct, "October Difference in Temperatures from 1948")
 
-# October From 1950 (Get rid of outliers from 1948 and 1949 for Arctic)
+# October From 1950 (Get rid of high temperature values from 1948 and 1949 for Arctic)
 temp_oct_1950 <- get_month_vals(34, lat_adjust, lon_adjust, air.array, time_length)
 plot(1951:2021, temp_oct_1950, xlab = "year", ylab = "temp difference (degC)", main = "October Difference in Temperatures from 1950")
 
@@ -132,20 +136,6 @@ max_years <- append(max_years, years_apr_dec[which.max(temp_oct)])
 max_years <- append(max_years, years_apr_dec[which.max(temp_nov)])
 max_years <- append(max_years, years_apr_dec[which.max(temp_dec)])
 
-max_vals <- rep()
-max_vals <- append(max_vals, max(temp_jan))
-max_vals <- append(max_vals, max(temp_feb))
-max_vals <- append(max_vals, max(temp_mar))
-max_vals <- append(max_vals, max(temp_apr))
-max_vals <- append(max_vals, max(temp_may))
-max_vals <- append(max_vals, max(temp_jun))
-max_vals <- append(max_vals, max(temp_jul))
-max_vals <- append(max_vals, max(temp_aug))
-max_vals <- append(max_vals, max(temp_sep))
-max_vals <- append(max_vals, max(temp_oct))
-max_vals <- append(max_vals, max(temp_nov))
-max_vals <- append(max_vals, max(temp_dec))
-
 # Also use which.min to find minimum years
 min_years <- rep()
 min_years <- append(min_years, years_jan_mar[which.min(temp_jan)])
@@ -161,48 +151,15 @@ min_years <- append(min_years, years_apr_dec[which.min(temp_oct)])
 min_years <- append(min_years, years_apr_dec[which.min(temp_nov)])
 min_years <- append(min_years, years_apr_dec[which.min(temp_dec)])
 
+# Max-min year plot
 df_years <- data.frame(1:12, max_years, min_years)
 ggplot(df_years, aes(1:12)) +
   geom_line(aes(y=max_years), color="red", size = 3) +
   geom_line(aes(y=min_years), color="blue", size = 3) +
   xlab("Month") + ylab("Year") + ggtitle("Years of Maximum (Red) and Minimum (Blue) Temperatures")
 
-# Get averages of temperature values for each month
-avg_temps <- rep()
-avg_temps <- append(avg_temps, mean(temp_jan))
-avg_temps <- append(avg_temps, mean(temp_feb))
-avg_temps <- append(avg_temps, mean(temp_mar))
-avg_temps <- append(avg_temps, mean(temp_apr))
-avg_temps <- append(avg_temps, mean(temp_may))
-avg_temps <- append(avg_temps, mean(temp_jun))
-avg_temps <- append(avg_temps, mean(temp_jul))
-avg_temps <- append(avg_temps, mean(temp_aug))
-avg_temps <- append(avg_temps, mean(temp_sep))
-# avg_temps <- append(avg_temps, mean(temp_oct))
-avg_temps <- append(avg_temps, mean(temp_oct_1950))
-avg_temps <- append(avg_temps, mean(temp_nov))
-avg_temps <- append(avg_temps, mean(temp_dec))
-
-# Get median of temperature values for each month
-median_temps <- rep()
-median_temps <- append(median_temps, median(temp_jan))
-median_temps <- append(median_temps, median(temp_feb))
-median_temps <- append(median_temps, median(temp_mar))
-median_temps <- append(median_temps, median(temp_apr))
-median_temps <- append(median_temps, median(temp_may))
-median_temps <- append(median_temps, median(temp_jun))
-median_temps <- append(median_temps, median(temp_jul))
-median_temps <- append(median_temps, median(temp_aug))
-median_temps <- append(median_temps, median(temp_sep))
-# median_temps <- append(median_temps, median(temp_oct))
-median_temps <- append(median_temps, median(temp_oct_1950))
-median_temps <- append(median_temps, median(temp_nov))
-median_temps <- append(median_temps, median(temp_dec))
-
 # Get standard deviation of temperature values for each month
 # The higher the standard deviation, the more varied the temperature differences are
-# Higher values during winter shows that climate change has higher impact on winter month temperatures
-# Summer isn't changing much -> More heat during winter
 sd_temps <- rep()
 sd_temps <- append(sd_temps, sd(temp_jan))
 sd_temps <- append(sd_temps, sd(temp_feb))
@@ -214,33 +171,16 @@ sd_temps <- append(sd_temps, sd(temp_jul))
 sd_temps <- append(sd_temps, sd(temp_aug))
 sd_temps <- append(sd_temps, sd(temp_sep))
 sd_temps <- append(sd_temps, sd(temp_oct))
-# sd_temps <- append(sd_temps, sd(temp_oct_1950))
 sd_temps <- append(sd_temps, sd(temp_nov))
 sd_temps <- append(sd_temps, sd(temp_dec))
+# Plot standard deviation vs. month
 plot(1:12, sd_temps, type = 'l', xlab = "Month", ylab = "Standard Deviation (degC)",
      main = "Temperature Standard Deviation over 12 Months", col = 'blue', lwd = '3')
 
-# Regression
+# Regression analysis for October
 years_1951 <- 1951:2021
-fit_jan <- lm(temp_jan ~ years_jan_mar)
-fit_feb <- lm(temp_feb ~ years_jan_mar)
-fit_mar <- lm(temp_mar ~ years_jan_mar)
-fit_apr <- lm(temp_apr ~ years_apr_dec)
-fit_may <- lm(temp_may ~ years_apr_dec)
-fit_jun <- lm(temp_jun ~ years_apr_dec)
-fit_jul <- lm(temp_jul ~ years_apr_dec)
-fit_aug <- lm(temp_aug ~ years_apr_dec)
-fit_sep <- lm(temp_sep ~ years_apr_dec)
-fit_oct <- lm(temp_oct ~ years_apr_dec)
-fit_oct_1951 <- lm(temp_oct_1950 ~ years_1951)
-fit_nov <- lm(temp_nov ~ years_apr_dec)
-fit_dec <- lm(temp_dec ~ years_apr_dec)
-
-# Residual Plot
-
-# Take year with most difference and 
-
-# Heat maps depicting difference
+fit_oct_1950 <- lm(temp_oct_1950 ~ years_1951)
+summary(fit_oct_1950)
 
 # Sea ice dataset
 nc_seaice <- nc_open("Z:/students/axie/project/G10010_V2/G10010_sibt1850_v2.0.nc")
@@ -248,14 +188,14 @@ lon <- ncvar_get(nc_seaice, "longitude")
 lat <- ncvar_get(nc_seaice, "latitude", verbose = F)
 t <- ncvar_get(nc_seaice, "time")
 
-seaice.array <- ncvar_get(nc_seaice, "seaice_conc") # store the data in a 3-dimensional array
+seaice.array <- ncvar_get(nc_seaice, "seaice_conc") # store data in 3D array
 fillvalue <- ncatt_get(nc_seaice, "seaice_conc", "_FillValue")
 
 nc_close(nc_mean_temp)
 
 # check which longitude values have ice
 seaice.slice <- seaice.array[, , 2000] # Adjust third index to see patterns in amount of seaice
-lon_adjust <- 100
+lon_adjust <- 100 # Pick constant longitude value
 ice_vals <- rep()
 for (x in 1:96) { # 96th index corresponds to 66.125 degrees North latitude
   ice_vals <- append(ice_vals, seaice.slice[lon_adjust, x])
